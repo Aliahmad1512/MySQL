119e64f3a69f8b2ae122d0424d3bd9905458f506
Ali Ahmad <88396377+Aliahmad1512@users.noreply.github.com>
2021-09-16 11:25:44 +0530

    Update HarryPotter_case5.sql

--- HarryPotter/HarryPotter_case5.sql
+++ HarryPotter/HarryPotter_case5.sql
@@ -25,9 +25,9 @@ group by power;
 select w.id, wp.age, w.coins_needed, w.power
 from wands w inner join wands_property wp on w.code = wp.code
 where w.coins_needed = (select min(coins_needed) 
-						from wands w1 inner join wands_property wp1 on w1.code = wp1.code
-						where wp1.is_evil = 0 and w.power = w1.power and wp.age = wp1.age)
-order by w.power desc, wp.age desc;
+			from wands w1 inner join wands_property wp1 on w1.code = wp1.code
+			where wp1.is_evil = 0 and w.power = w1.power and wp.age = wp1.age) 
+			order by w.power desc, wp.age desc;
 
 
 
